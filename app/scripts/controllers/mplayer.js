5228f241b9c7fb8539d51d01c403976681b3c331
Matt Nodurfth <mnodurft@gmail.com>
2013-10-18 00:17:03 +0800

    Working mplayer, need to fix: resetGame(turnNum, array?, .endGame, cat?), play again button?, cat game?

--- app/scripts/controllers/mplayer.js
+++ app/scripts/controllers/mplayer.js
@@ -1,16 +1,18 @@
 'use strict';
 
 angular.module('TicketyApp')
-  .controller('MplayerCtrl', function ($scope, $routeParams, angularFire) {
+  .controller('MplayerCtrl', function ($scope, $routeParams, angularFire, $timeout) {
     $scope.mySymbol = $routeParams.mySymbol;    
     $scope.myTurn = false;
+    $scope.turnNum = 0;
     
-    $scope.gameBoard = [];
+    $scope.gameBoard = [" "," "," "," "," "," "," "," "," "];
     var gameBoardRef = new Firebase("https://hkwdi1-inst.firebaseio.com/room/" + $routeParams.id);
     $scope.promise = angularFire(gameBoardRef, $scope, "gameBoard", []);
 
     $scope.promise.then (function () {
-      $scope.gameBoard = [];
+      // $scope.gameBoard = [];
+      $scope.gameBoard = [" "," "," "," "," "," "," "," "," "];
       if ($routeParams.mySymbol == 'x') {
         console.log("I am First Move: Symbol: " + $routeParams.mySymbol);
         $scope.myTurn = true;
@@ -26,14 +28,12 @@ angular.module('TicketyApp')
         if (snapshot.val() != null) {
           if (!arrays_equal(snapshot.val(), $scope.gameBoard)) {
             console.log("diff gameboard");
-            if ($scope.isLosing()) {
-              // print losing
-              // redirect to match player if play again
-            } else if ($scope.isDraw()) {
-              // print draw
-              // redirect to match player if play again
+            if ($scope.testForWin()) {
+            		//do some stuff?
+            		console.log("L:AKJSFL:KJASFLKJSDFL WE WONNNNN!")
             } else {
               $scope.myTurn = true;
+              $scope.turnNum++;
             }
           } else {
             console.log("same gameboard"); 
@@ -51,42 +51,29 @@ angular.module('TicketyApp')
     $scope.handleClick = function(index) {
       if ($scope.myTurn) {
         $scope.gameBoard[index] = $scope.mySymbol;
-      
-        if ($scope.isWinning()) {
-          // print winning
-          // redirect to match player if play again
-        } else if ($scope.isDraw()) {
-          // print draw
-          // redirect to match player if play again
-        } else {
+      	$scope.turnNum++;
+      	console.log("turn # " + $scope.turnNum + "$scope.gameBoard: " + $scope.gameBoard.join(""))
+         if ($scope.testForWin()) {
+          $timeout(function() {window.alert($scope.mySymbol.toUpperCase() + ' won!');}, 50);
+        }  else {
           $scope.myTurn = false;
         }
       }
     }
     
-    $scope.isLosing = function() {
-      return false; 
-    }
-    
-    $scope.isWinning = function() {
-      return false; 
-    }
-    
-    $scope.isDraw = function() {
-      return false; 
-    }    
     
     function arrays_equal(a,b) { return !(a<b || b<a); }
-  });
+
 
 
 //////////////////////////// Shared functions => service!
  $scope.testForWin = function() {
-      $scope.squaresStr = $scope.squares.join("");
+ 			console.log("hi testForWin has been fired!")
+      $scope.gameBoardStr = $scope.gameBoard.join("");
       var patt1=/\s/g;
-      var properStr = $scope.squaresStr.replace(patt1, '.');
-      
-      if (turnNum % 2 === 0) {
+      var properStr = $scope.gameBoardStr.replace(patt1, '.');
+      console.log("I'm gameboard string: " + properStr)
+      if ($scope.turnNum % 2 != 0) {
         var patterns = X_WIN_PATTERNS;
       } else {
         var patterns = O_WIN_PATTERNS;
@@ -94,16 +81,36 @@ angular.module('TicketyApp')
 
       for (var i = 0; i < patterns.length; i++) {
         var re = new RegExp(patterns[i], "i");
+        console.log(patterns[i] + " BREAK " + properStr.match(re))
+        console.log("I'm match checking " + properStr.match(re));
         if (properStr.match(re)){
+
           $scope.endGame = true;
+          console.log("L:AKJSFL:KJASFLKJSDFL WE WONNNNN!")
           return true;
         };
       };
 
-      if (turnNum+1 === 9) { 
+      if ($scope.turnNum+1 === 9) { 
         $scope.endGame = true;
-        $scope.mark = 'Cat';
+        $scope.mySymbol = 'Cat';
+
       }
 
       return false;
   }
+
+  var X_WIN_PATTERNS = [
+          'xxx......',
+          '...xxx...',
+          '......xxx',
+          'x..x..x..',
+          '.x..x..x.',
+          '..x..x..x',
+          'x...x...x',
+          '..x.x.x..'
+        ];
+    var O_WIN_PATTERNS = X_WIN_PATTERNS.map(function(str){ return str.replace(/x/g, 'o');});
+
+
+    });
